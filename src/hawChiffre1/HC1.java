63eff964fd69b7adda7c3cc049f828875e10febe
BAAMMM1 <BAAMMM@outlook.de>
2017-04-25 20:06:17 +0200

    HC1 - mit LCG - mit SecureRandom
    erstellt

--- src/hawChiffre1/HC1.java
+++ src/hawChiffre1/HC1.java
@@ -1,5 +1,74 @@
 package hawChiffre1;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.SecureRandom;
+
+import lCGenerator.LCG;
+
 public class HC1 {
+	
+	private String pathIn;
+	private String pathOut;
+	private IOController controller;
+	private LCG lcg;
+	private java.security.SecureRandom secureRandom;
+
+	public HC1(int startValue, String pathIn, String pathOut){
+		this.pathIn = pathIn;
+		this.pathOut = pathOut;
+		this.controller = new IOController();
+		this.lcg = new LCG(startValue);
+		this.secureRandom = new SecureRandom();
+		this.secureRandom.setSeed(startValue);
+	}
+	
+	public void encryptFileWithLCG() throws FileNotFoundException, IOException{
+		byte[] file = controller.readFile(this.pathIn);
+		
+		for(int i = 0; i < file.length; i++){
+			int schluessel = this.lcg.nextInt();
+			file[i] = (byte) (file[i] ^ schluessel);
+		}
+		
+		
+		this.controller.writeFile(file, pathOut);
+	}
+	
+	public void encryptFileWithSecureRandom() throws FileNotFoundException, IOException{
+		byte[] file = controller.readFile(this.pathIn);
+		
+		for(int i = 0; i < file.length; i++){
+			int schluessel = this.secureRandom.nextInt();
+			file[i] = (byte) (file[i] ^ schluessel);
+		}
+		
+		
+		this.controller.writeFile(file, pathOut);
+	}
+	
+	
+	
+	
+	 public static void main(String[] args){
+		if(args.length == 3) {
+			int startValue = Integer.valueOf(args[0]).intValue();
+			String pathIn = args[1];
+			String pathOut = args[2];
+			HC1 hc1 = new HC1(startValue, pathIn, pathOut);
+			
+			try {
+				hc1.encryptFileWithLCG();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			
+		} else {
+			throw new IllegalArgumentException("Parameter: Startwert, In-Dateipfad, Out-Dateipfad");
+		}
+	}
 
 }
---
+++ src/test/TestHC1.java
@@ -0,0 +1,75 @@
+package test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import hawChiffre1.HC1;
+import hawChiffre1.IOController;
+import utility.Printer;
+
+public class TestHC1 {
+
+	private static final int START_VALUE = 1;
+	private static final String TEST_LCG_FILE_PATH = "Test1.txt";
+	private static final String TEST_LCG_FILE_ENCRYPT_PATH = "Test1EnCrpyt.txt";
+	private static final String TEST_LCG_FILE_DECRYPT_PATH = "Test1DECrpyt.txt";
+	
+	private static final String TEST_SECURE_RANDOM_FILE_PATH = "Test2.txt";
+	private static final String TEST_SECURE_RANDOM_FILE_ENCRYPT_PATH = "Test2EnCrpyt.txt";
+	private static final String TEST_SECURE_RANDOM_FILE_DECRYPT_PATH = "Test2DECrpyt.txt";
+
+	public TestHC1() {
+
+	}
+
+	public void startTest() {
+		Printer.prompt(this, "Starte HC1 Test");
+		IOController controller = new IOController();
+		HC1 hc1LCG = new HC1(START_VALUE, TEST_LCG_FILE_PATH, TEST_LCG_FILE_ENCRYPT_PATH);
+		HC1 hc2LCG = new HC1(START_VALUE, TEST_LCG_FILE_ENCRYPT_PATH, TEST_LCG_FILE_DECRYPT_PATH);
+		
+		HC1 hc1SecureRandom = new HC1(START_VALUE, TEST_LCG_FILE_PATH, TEST_LCG_FILE_ENCRYPT_PATH);
+		HC1 hc2SecureRandom = new HC1(START_VALUE, TEST_LCG_FILE_ENCRYPT_PATH, TEST_LCG_FILE_DECRYPT_PATH);
+		
+		try {
+			/*
+			 * Ausführen mit der selber geschriebenen LCG Klasse
+			 */
+			hc1LCG.encryptFileWithLCG();
+			hc2LCG.encryptFileWithLCG();
+
+			String fileLCG_Defualt = new String(controller.readFile(TEST_LCG_FILE_PATH));
+			String fileLCG_DeEnCrypt = new String(controller.readFile(TEST_LCG_FILE_DECRYPT_PATH));
+
+			Printer.prompt(this, "File sind gleich: " + fileLCG_Defualt.equals(fileLCG_DeEnCrypt) + " mit LCG");
+			
+			/*
+			 * Ausführung mit der SecureRandom Klasse
+			 */
+			hc1SecureRandom.encryptFileWithLCG();
+			hc2SecureRandom.encryptFileWithLCG();
+
+			String fileSecureRandom_Default = new String(controller.readFile(TEST_SECURE_RANDOM_FILE_PATH));
+			String fileSecureRandom_DeEnCrypt = new String(controller.readFile(TEST_SECURE_RANDOM_FILE_DECRYPT_PATH));
+
+			Printer.prompt(this, "File sind gleich: " + fileSecureRandom_Default.equals(fileSecureRandom_DeEnCrypt) + " mit SecureRandom");
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Printer.prompt(this, "Beende HC1 Test");
+	}
+
+	public static void main(String[] args) {
+		TestHC1 testHC1 = new TestHC1();
+		testHC1.startTest();
+	}
+
+	@Override
+	public String toString() {
+		return String.format("TestHC1");
+	}
+
+}

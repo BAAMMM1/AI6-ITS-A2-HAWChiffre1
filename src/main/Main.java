7df8c0f373548e9b57bdef2af10073338bb56973
BAAMMM1 <BAAMMM@outlook.de>
2017-04-25 20:12:07 +0200

    Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,21 +9,6 @@ public class Main {
 
 	
 	public static void main(String[] args){
-		IOController controller = new IOController();	
-		try {
-			byte[] file1 = controller.readFile("Test1.txt");
-			byte[] file2 = controller.readFile("Test1DeChiff.txt");
-			
-			String fileString1 = new String(file1);
-			String fileString2 = new String(file2);
-			
-			System.out.println(fileString1);
-			System.out.println(fileString2);
-			System.out.println(fileString1.equals(fileString2));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 	}
 

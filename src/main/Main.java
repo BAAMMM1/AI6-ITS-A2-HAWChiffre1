541bef7e1e7653bfedd748c2bfc620404e8f3f15
BAAMMM1 <BAAMMM@outlook.de>
2017-04-27 10:56:42 +0200

    Abgabe

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,32 @@
 package main;
 
-public class Main {
-	
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-	
-	public static void main(String[] args){
-		
+import hawChiffre1.HC1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		if(args.length == 3) {
+			int startValue = Integer.valueOf(args[0]).intValue();
+			String pathIn = args[1];
+			String pathOut = args[2];
+			HC1 hc1 = new HC1();
+			
+			try {
+				hc1.cryptFile(startValue, pathIn, pathOut, true);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			
+		} else {
+			throw new IllegalArgumentException("Parameter: Startwert, In-Dateipfad, Out-Dateipfad");
+		}
+
 	}
 
 }
